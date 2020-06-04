d4c8def3930780da83fa60ad0686cee413a08685
Andres Campuzano <campuxano88@gmail.com>
2020-06-03 22:47:08 -0500

    Changed from class to component.

--- src/App.js
+++ src/App.js
@@ -1,51 +1,66 @@
-import React, { Component } from 'react';
-import './App.css';
-import Tests01 from './Tests01';
-// import TestComponent from './TestComponent';
+import React, { useState } from 'react';
+import './styles.css';
 
 // const API_KEY = '9cae4d09a666e110149f7047739076f5';
 // const temperatureUnit = 'metric';
 // const inputVal = 'busan';
 
-// #V882JV0U
+// https://api.openweathermap.org/data/2.5/weather?q=busan&appid=c982d29f34b457c7256dfdf78eff3288&units=metric
 
-class App extends Component {
-	state = {
-		data: {},
-	};
+const App = () => {
+  const [data, setData] = useState({
+    data: {},
+    input: ''
+  });
 
-	componentDidMount() {
-		this.fetchData();
-	}
+  const handleClick = async () => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${data.input}&appid=c982d29f34b457c7256dfdf78eff3288&units=metric`
+    )
+      .then(response => response.json())
+      .then(data => {
+        setData({
+          data: data
+        });
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
 
-	fetchData = async () => {
-		const response = await fetch(
-			`https://api.openweathermap.org/data/2.5/weather?q=busan&appid=c982d29f34b457c7256dfdf78eff3288&units=metric`
-		);
-		const data = await response.json();
-		this.setState({
-			data: data,
-		});
-		console.log(data);
-		console.log(data.weather[0]['id']);
-		console.log(data.weather[0]['description']);
-		console.log(this.state.data.weather[0]['description']);
-		console.log(this.state.data.main.temp);
-	};
+  const handleInput = e => {
+    setData({
+      input: e.target.value
+    });
+  };
 
-	render() {
-		return (
-			<div className='App'>
-				<h1>Test from app {this.state.data.name}</h1>
-				<p>
-					{Object.keys(this.state.data).length
-						? this.state.data.weather[0]['description']
-						: ''}
-				</p>
-				{/* <Tests01 propTest={this.state.data.main} /> */}
-			</div>
-		);
-	}
-}
+  const handleKeyPress = e => {
+    if (e.key === 'Enter') {
+      handleClick();
+    }
+  };
+
+  return (
+    <div className='App'>
+      <input type='text' onChange={handleInput} onKeyPress={handleKeyPress} />
+      <button type='button' onClick={handleClick}>
+        Bring data
+      </button>
+      {data.data ? (
+        <div>
+          <h1>City => {data.data.name}</h1>
+          <p>
+            {Object.keys(data.data).length
+              ? data.data.weather[0]['description']
+              : ''}
+          </p>
+        </div>
+      ) : (
+        <p>Not found</p>
+      )}
+    </div>
+  );
+};
 
 export default App;
